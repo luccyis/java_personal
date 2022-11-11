c1e4711b6817148c84c48baec54e39bc43599e80
ezen <ezen@DESKTOP-O03MT84>
2022-11-11 16:15:25 +0900

    코딩테스트 연습

---
+++ src/java_personal/tt/codingTest/Nov11/HelloWorld.java
@@ -0,0 +1,17 @@
+package java_personal.tt.codingTest.Nov11;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a , b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a-b);
+	}
+	
+}
