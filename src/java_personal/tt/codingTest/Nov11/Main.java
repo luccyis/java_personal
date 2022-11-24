8d87648713e3a3a79a6bf5fe875fec027279d9bc
luccyis <mungjee08@naver.com>
2022-11-24 18:43:54 +0900

    코딩테스트연습

--- src/java_personal/tt/codingTest/Nov11/Main.java
+++ src/java_personal/tt/codingTest/Nov11/Main.java
@@ -7,11 +7,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a , b;
-		  
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println();
+		int a = sc.nextInt();
+		String b = sc.next();
+		
+		int c = Integer.parseInt(b.substring(0,1));
+		int d = Integer.parseInt(b.substring(1,2));
+		int e = Integer.parseInt(b.substring(2,3));
+		
+		int cde = Integer.parseInt(b);
+		
+		System.out.println(a*e);
+		System.out.println(a*d);
+		System.out.println(a*c);
+		System.out.println(a*cde);
+		
+		System.out.println("        ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println("\\. \". L_r'");
+		System.out.println("  `~\\/");
+		System.out.println("     |");
+		System.out.println("     |");
+
 	}
 
 }
